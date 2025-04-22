0b0f388d6d9fe5a3c1e3cc27741c264f071ebc6e
azraoykulukan <azraulukan@gmail.com>
2025-04-22 19:31:50 +0300

    Tüm Entityler Oluşturulup Tamamlandı.

---
+++ Villa.Entity/Entities/QA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Entity.Entities
+{
+    //4. VİDEO 4.40 DK CLASS İSMİ QUEST BEN QA YAPTIM!!
+    public class QA : BaseEntity 
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
