2970e63f97b3fe37a2c3a14f0dfc3b05ab8f8f25
azraoykulukan <azraulukan@gmail.com>
2025-05-03 23:46:30 +0300

    Business GenericManager ve Concrete Classlar tamamlandı

---
+++ Villa.Business/Concrete/CounterManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Business.Abstract;
+using Villa.DataAccess.Abstract;
+using Villa.Entity.Entities;
+
+namespace Villa.Business.Concrete
+{
+    public class CounterManager : GenericManager<Counter>, ICounterService
+    {
+        public CounterManager(IGenericDal<Counter> genericDal) : base(genericDal)
+        {
+        }
+    }
+}
