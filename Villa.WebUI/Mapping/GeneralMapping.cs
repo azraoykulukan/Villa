2b4518386be1bdff241124448eecc4ad0a08222e
azraoykulukan <azraulukan@gmail.com>
2025-05-08 02:02:04 +0300

    Dto'lar tamamlandı. Mappingler yapıldı.

--- Villa.WebUI/Mapping/GeneralMapping.cs
+++ Villa.WebUI/Mapping/GeneralMapping.cs
@@ -2,6 +2,13 @@
 using Villa.Dto.Dtos.BannerDtos;
 using Villa.Dto.Dtos.ContactDtos;
 using Villa.Dto.Dtos.CounterDtos;
+using Villa.Dto.Dtos.DealDtos;
+using Villa.Dto.Dtos.FeatureDtos;
+using Villa.Dto.Dtos.MessageDtos;
+using Villa.Dto.Dtos.ProductDtos;
+using Villa.Dto.Dtos.QuestDtos;
+using Villa.Dto.Dtos.SubHeaderDtos;
+using Villa.Dto.Dtos.VideoDtos;
 using Villa.Entity.Entities;
 
 namespace Villa.WebUI.Mapping
@@ -22,9 +29,34 @@ namespace Villa.WebUI.Mapping
             CreateMap<UpdateCounterDto, Counter>().ReverseMap();
             CreateMap<CreateCounterDto, Counter>().ReverseMap();
 
-            CreateMap<ResultCounterDto, Counter>().ReverseMap();
-            CreateMap<UpdateCounterDto, Counter>().ReverseMap();
-            CreateMap<CreateCounterDto, Counter>().ReverseMap();
+            CreateMap<CreateDealDto, Deal>().ReverseMap();
+            CreateMap<UpdateDealDto, Deal>().ReverseMap();
+            CreateMap<ResultDealDto, Deal>().ReverseMap();
+
+            CreateMap<ResultFeatureDto, Feature>().ReverseMap();
+            CreateMap<CreateFeatureDto, Feature>().ReverseMap();
+            CreateMap<UpdateFeatureDto, Feature>().ReverseMap();
+
+            CreateMap<ResultMessageDto, Message>().ReverseMap();
+            CreateMap<UpdateMessageDto, Message>().ReverseMap();
+            CreateMap<CreateMessageDto, Message>().ReverseMap();
+
+            CreateMap<ResultProductDto, Product>().ReverseMap();
+            CreateMap<CreateProductDto, Product>().ReverseMap();
+            CreateMap<UpdateProductDto, Product>().ReverseMap();
+
+            CreateMap<ResultQuestDto, Quest>().ReverseMap();
+            CreateMap<UpdateQuestDto, Quest>().ReverseMap();
+            CreateMap<CreateQuestDto, Quest>().ReverseMap();
+
+            CreateMap<ResultVideoDto, Video>().ReverseMap();
+            CreateMap<CreateVideoDto, Video>().ReverseMap();
+            CreateMap<UpdateVideoDto, Video>().ReverseMap();
+
+          /*  CreateMap<ResultSubHeaderDto, SubHeader>().ReverseMap();
+            CreateMap<CreateSubHeaderDto, SubHeader>().ReverseMap();
+            CreateMap<UpdateSubHeaderDto, SubHeader>().ReverseMap(); */
+
         }
     }
 }
